838b34097822991a839634272197c4264d60fd31
alifaa12 <fadillaaa69@gmail.com>
2022-02-10 14:33:07 +0700

    tugas5

---
+++ nomor2.js
@@ -0,0 +1,35 @@
+// function dropRight(number) {
+//     const removed = number.filter(el => {
+//         return el <= 2
+//     })
+//     console.log(removed);
+// }
+// dropRight([1,2,3,],2)
+
+// function dropRight(number) {
+//     const removed = number.filter(el => {
+//         return el < 2
+//     })
+//     console.log(removed);
+// }
+// dropRight([1,2,3,],2)
+
+// function dropRight(number) {
+//     const removed = number.filter(el => {
+//         return el < 1
+//     })
+//     console.log(removed);
+// }
+// dropRight([1,2,3,],5)
+
+
+function dropRight(number) {
+    const removed = number.filter(el => {
+        return el => 2
+    })
+    console.log(removed);
+}
+dropRight([1,2,3,],0)
+
+
+
